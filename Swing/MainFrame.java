48f7eceb14634e766e0731d599638530a90f6750
HossainMaruf <maruficepustian@gmail.com>
2025-07-28 13:16:53 +0600

    3

--- Swing/MainFrame.java
+++ Swing/MainFrame.java
@@ -1,11 +1,20 @@
 import javax.swing.*;
 
 public class MainFrame {
-    JFrame frame = null; 
+    JFrame frame = null;
+    JButton button = null;
+
     public MainFrame() {
         frame = new JFrame("App"); // create a frame or GUI
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit the program when close the window
         frame.setSize(400, 300); // set the frame Dimension
+        frame.setLayout(null); // default is BorderLayout for JFrame, need to set null for manual positioning using setBounds()
+
+        // Adding the design
+        button = new JButton("Click Me");
+        button.setBounds(0, 20, 80, 40);
+        frame.add(button);
+
         frame.setVisible(true); // show the frame
     }
 
