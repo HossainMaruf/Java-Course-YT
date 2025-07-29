1ac976f8edd4225ef108d1ff4bcd41b451e3ea68
HossainMaruf <maruficepustian@gmail.com>
2025-07-29 12:00:17 +0600

    6 Added JPanel and Basic of BorderLayout of Frame

--- Swing/MainFrame.java
+++ Swing/MainFrame.java
@@ -1,39 +1,56 @@
 import javax.swing.*;
+
+import java.awt.BorderLayout;
 import java.awt.event.*;
 
 public class MainFrame {
     JFrame frame = null;
-    JButton button = null;
-    JTextField textField = null;
-    JLabel label = null;
+    JPanel namePanel = null, passwordPanel = null;
+    JLabel nameLabel = null, passwordLabel = null;
+    JTextField nameField = null, passwordField = null;
+    JButton nameBtn = null, passwordBtn = null;
 
     public MainFrame() {
         frame = new JFrame("App"); // create a frame or GUI
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit the program when close the window
         frame.setSize(400, 300); // set the frame Dimension
-        frame.setLayout(null); // default is BorderLayout for JFrame, need to set null for manual positioning
-                               // using setBounds()
 
-        // Creating object for the MainFrame
-        label = new JLabel("Message");
-        textField = new JTextField();
-        button = new JButton("Click Me");
-        // Setting size and position of the objects
-        label.setBounds(0, 0, 70, 30);
-        textField.setBounds(72, 0, 80, 30);
-        button.setBounds(155, 0, 80, 30);
+        // Creating panel
+        namePanel = new JPanel();
+        passwordPanel = new JPanel();
+        // Creating object for the panel
+        nameLabel = new JLabel("Name");
+        passwordLabel = new JLabel("Password");
+        nameField = new JTextField(20);
+        passwordField = new JTextField(20);
+        nameBtn = new JButton("Check Name");
+        passwordBtn = new JButton("Check Password");
+      
 
         // Adding action listener to the button
-        button.addActionListener(new ActionListener() {
+        nameBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // JOptionPane.showMessageDialog(frame, "Button was clicked!");
-                JOptionPane.showMessageDialog(frame, textField.getText());
+                JOptionPane.showMessageDialog(frame, nameField.getText());
             }
         });
 
-        frame.add(label);
-        frame.add(textField);
-        frame.add(button);
+        passwordBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // JOptionPane.showMessageDialog(frame, "Button was clicked!");
+                JOptionPane.showMessageDialog(frame, passwordField.getText());
+            }
+        });
+
+        namePanel.add(nameLabel);
+        namePanel.add(nameField);
+        namePanel.add(nameBtn);
+        frame.add(namePanel, BorderLayout.NORTH);
+
+        passwordPanel.add(passwordLabel);
+        passwordPanel.add(passwordField);
+        passwordPanel.add(passwordBtn);
+        frame.add(passwordPanel, BorderLayout.CENTER);
 
         frame.setVisible(true); // show the frame
     }
