19cb30f132f309f15c60c989ec96f867932a616f
HossainMaruf <maruficepustian@gmail.com>
2025-07-29 16:19:42 +0600

    Basic Todo App

--- Swing/MainFrame.java
+++ Swing/MainFrame.java
@@ -5,62 +5,43 @@ import java.awt.event.*;
 
 public class MainFrame {
     JFrame frame = null;
-    JPanel namePanel = null, passwordPanel = null;
-    JLabel nameLabel = null, passwordLabel = null;
-    JTextField nameField = null;
-    JPasswordField passwordField = null;
-    JButton nameBtn = null, passwordBtn = null;
+    JPanel inputPanel = null, todosPanel = null;
+    JTextField todo = null;
+    JButton addBtn = null, dltBtn = null;
 
     public MainFrame() {
-        frame = new JFrame("App"); // create a frame or GUI
+        frame = new JFrame("Todo App"); // create a frame or GUI
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit the program when close the window
         frame.setSize(600, 300); // set the frame Dimension
 
         // Creating panel
-        namePanel = new JPanel();
-        passwordPanel = new JPanel();
+        inputPanel = new JPanel();
+        todosPanel = new JPanel();
         // Creating object for the panel
-        nameLabel = new JLabel("Name");
-        passwordLabel = new JLabel("Password");
-        nameField = new JTextField(20);
-        passwordField = new JPasswordField(20);
-        nameBtn = new JButton("Check Name");
-        passwordBtn = new JButton("Check Password");
-      
+        todo = new JTextField(20);
+        addBtn = new JButton("Add");
+
+
+        inputPanel.add(todo);
+        inputPanel.add(addBtn);
+        frame.add(inputPanel, BorderLayout.NORTH);
+
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        JList<String> list = new JList<String>(listModel);
+        JScrollPane scrollPane = new JScrollPane(list);
+        frame.add(scrollPane, BorderLayout.CENTER);
+
 
         // Adding action listener to the button
-        nameBtn.addActionListener(new ActionListener() {
+        addBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String name = nameField.getText();
-                if(name.equals("")) return;
-                else {
-                    // TODO: validation rule is not enough now
-                    JOptionPane.showMessageDialog(frame, "valid");
+                String text = todo.getText().trim();
+                if (!text.isEmpty()) {
+                    listModel.addElement(text); // Add to model
+                    todo.setText("");     // Clear input field
                 }
             }
         });
-
-        passwordBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                char[] charPassword = passwordField.getPassword();
-                String password = new String(charPassword);
-                if(password.equals("")) return;
-                else {
-                    // TODO: validation rule is not enough now
-                    JOptionPane.showMessageDialog(frame, "valid");
-                }
-            }
-        });
-
-        namePanel.add(nameLabel);
-        namePanel.add(nameField);
-        namePanel.add(nameBtn);
-        frame.add(namePanel, BorderLayout.NORTH);
-
-        passwordPanel.add(passwordLabel);
-        passwordPanel.add(passwordField);
-        passwordPanel.add(passwordBtn);
-        frame.add(passwordPanel, BorderLayout.CENTER);
 
         frame.setVisible(true); // show the frame
     }
